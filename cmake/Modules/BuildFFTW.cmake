fc8eb614f06b9a6bd85a9f626cd9bb840b76a175
Francois <francois@orioncloud.fr>
2018-04-01 13:00:35 -0400

    Should compile now

--- cmake/Modules/BuildFFTW.cmake
+++ cmake/Modules/BuildFFTW.cmake
@@ -6,7 +6,7 @@ set(FFTWMD5 0d5915d7d39b3253c1cc05030d79ac47)
 find_package(FFTW)
 
 # If GSL is not installed, lets go ahead and compile it
-if(NOT FFTW_FOUND )
+if(NOT FFTW_FOUND)
     message(STATUS "FFTW not found, downloading and compiling from source")
     ExternalProject_Add(FFTW
         PREFIX FFTW
@@ -19,5 +19,5 @@ if(NOT FFTW_FOUND )
         BUILD_IN_SOURCE 1)
         set(FFTW_LIBRARY_DIRS ${CMAKE_BINARY_DIR}/extern/lib/ )
         set(FFTW_INCLUDES ${CMAKE_BINARY_DIR}/extern/include/)
-        set(FFTW_LIBRARIES -lfftw3)
+        set(FFTW_LIBRARIES -lfftw3f)
 endif()
